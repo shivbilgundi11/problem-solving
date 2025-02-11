746a418527b1bf01ab9462ac30ae4ec5836162b6
Shivaprasad Bilgundi <shivbbilgundi11@gmail.com>
2025-02-11 15:09:11 +0530

    probSolve: find the number of chars in array of mixed data types

--- main.js
+++ main.js
@@ -79,3 +79,18 @@ function calChars(str) {
   });
   console.log(rsltObj);
 }
+
+// Task 6...!
+const randNumschars = [3, "a", "a", "a", 2, 3, "a", 3, "a", 2, 4, 9, 3];
+
+function countChars(str, char) {
+  const rsltObj = {};
+
+  str.forEach((val) => {
+    rsltObj[val] = (rsltObj[val] || 0) + 1;
+  });
+
+  console.log(rsltObj[char]);
+
+  console.log(`${char} appeared ${rsltObj[char]} times...!`);
+}
